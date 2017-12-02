620aec58073b66eecf5d2d3b7c1d47daa686b8e5
slawiko <shchaurouski.slava@gmail.com>
2017-12-02 19:37:00 +0300

    wc bugfix

--- wc/main.c
+++ wc/main.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-void free_save(char* buffer) {
-    if (buffer) {
-        free(buffer);
+#ifdef _WIN32
+# include <io.h>
+# include <fcntl.h>
+# define SET_BINARY_MODE(handle) setmode(handle, O_BINARY)
+#else
+# define SET_BINARY_MODE(handle) ((void)0)
+#endif
+
+int digit_length(unsigned long long x) {
+    int counter = 0;
+
+    if (x == 0) {
+        return 1;
     }
+    while(x != 0) {
+        x /= 10;
+        ++counter;
+    }
+
+    return counter;
 }
 
-unsigned long long read_everything(char** buffer) {
-    int c;
-    unsigned long long i = 0;
-
-    c = getchar();
-    while (c != EOF) {
-        if (i == ULONG_LONG_MAX) {
-            free_save(*buffer);
-            return 0;
-        }
-        *buffer = (char *)realloc(*buffer, i + 1);
-        if (*buffer == NULL) {
-            free_save(*buffer);
-            return 0;
-        }
-        (*buffer)[i++] = c;
-
-        c = getchar();
+int max_in_three(int x, int y, int z) {
+    int max = x;
+    if (y > max) {
+        max = y;
     }
-
-    return i;
+    if (z > max) {
+        max = z;
+    }
+    return max;
 }
 
 int main() {
-    char* buffer = NULL;
-    unsigned long long bytes = read_everything(&buffer);
+    unsigned long long line_count = 0;
+    unsigned long long word_count = 0;
+    unsigned long long byte_count = 0;
 
-    printf("%llu\n", bytes);
-    printf("%s", buffer);
+    int cur;
+    bool in_word = false;
 
-    free_save(buffer);
+    SET_BINARY_MODE(fileno(stdin));
+
+    cur = getchar();
+    while (cur != EOF) {
+        byte_count++;
+
+        if (cur == '\n') {
+            line_count++;
+        }
+
+        if (!in_word && isprint(cur) && !isspace(cur)) {
+            in_word = true;
+        } else if (in_word && isspace(cur)) {
+            in_word = false;
+            word_count++;
+        }
+
+        cur = getchar();
+    }
+    if (in_word) {
+        word_count++;
+    }
+
+    int l = digit_length(line_count);
+    int w = digit_length(word_count);
+    int b = digit_length(byte_count);
+    int max = max_in_three(l, w, b);
+
+    printf("%*s%llu %*s%llu %*s%llu\n", max - l, "", line_count, max - w, "", word_count, max - b, "", byte_count);
+
     return 0;
 }
