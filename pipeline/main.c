6759bc82301e0cc30bbae1c235dcb9cd5b920781
slawiko <shchaurouski.slava@gmail.com>
2018-04-16 01:11:17 +0300

    pipeline working

--- pipeline/main.c
+++ pipeline/main.c
@@ -2,47 +2,62 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-char* ARGS[2][5] = {
-        { "ls", "-l", "/etc/", NULL },
-        { "wc", "-l", NULL }
+char* ARGS[3][5] = {
+        {"ls", "-l", "/etc/", NULL},
+        {"grep", "p", NULL},
+        {"wc", "-l", NULL}
 };
 
 int main() {
-    int pipefds[2];
-    if (pipe(pipefds) == -1) {
-        perror("pipe");
-        return 1;
-    }
+    int n = 3;
+    int fds_cnt = n - 1;
 
-    pid_t children[2];
+    int pipefds[fds_cnt][2];
 
-    for (int i = 0; i < 2; ++i) {
+    pid_t children[n];
+
+    for (int i = 0; i < n; ++i) {
+        if (i < fds_cnt && pipe(pipefds[i]) == -1) {
+            perror("pipe");
+            return 1;
+        }
+
         pid_t pid = fork();
 
+        if (pid < 0) {
+            perror("Child process creation failed");
+            return 1;
+        }
         if (pid == 0) {
             char** args = ARGS[i];
             if (i == 0) {
-                dup2(pipefds[1], 1);
-                //close(pipefds[1]);
-                close(pipefds[0]);
-            } else if (i == 1) {
-                dup2(pipefds[0], 0);
-                //close(pipefds[0]);
-                close(pipefds[1]);
+                dup2(pipefds[i][1], 1);
+
+                close(pipefds[i][0]);
+            } else if (i == fds_cnt) {
+                dup2(pipefds[i - 1][0], 0);
+
+                close(pipefds[i - 1][1]);
+            } else {
+                dup2(pipefds[i][1], 1);
+                dup2(pipefds[i - 1][0], 0);
+
+                close(pipefds[i][0]);
+                close(pipefds[i - 1][1]);
             }
             execvp(args[0], args);
         } else {
             children[i] = pid;
+            if (i > 0) {
+                close(pipefds[i - 1][0]);
+                close(pipefds[i - 1][1]);
+            }
         }
-    }
-
-    for (int i = 0; i < 2; ++i) {
-        close(pipefds[i]);
     }
 
     int exitCode = 0;
     int status;
-    for (int i = 0; i < 2; ++i) {
+    for (int i = 0; i < n; ++i) {
         waitpid(children[i], &status, 0);
         if (WIFEXITED(status)) {
             if (WEXITSTATUS(status) != 0) {
